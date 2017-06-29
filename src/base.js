0b8832016e161185555cb91d53d46f85997a97f6
Dallen Loder <dllnldr@gmail.com>
2017-06-29 17:40:02 -0600

    first 20 lessons done

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyADscNhqkvQFgOdVjuDyYw5GmIk6T4nsRI",
+  authDomain: "catch-of-the-day-dallen-loder.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-dallen-loder.firebaseio.com",
+});
+
+export default base;
